e5df909a5d4c837f5582fa75fd16cf1271917828
wangxy <wangxy@weipaitang.com>
2019-10-16 17:29:42 +0800

    实例后调用

--- demo/test.js
+++ demo/test.js
@@ -1,26 +1,55 @@
-function lg(...props){
-    var a = 0;
-   return  console.log(...props,a++)
+// function lg(...props){
+//     var a = 0;
+//    return  console.log(...props,a++)
 
 
-}
+// }
 
-function fn(){}
+// function fn(){}
 
-const obj = new fn(); 
+// const obj = new fn(); 
 
-lg( obj.__proto__ === fn  )  //false
-
-
-lg(obj.__proto__===fn.prototype)//true
-lg(fn.prototype.constructor=obj)//false   fn
-
-// lg(fn instanceof obj )//error
+// lg( obj.__proto__ === fn  )  //false
 
 
-lg(obj instanceof fn)//true
+// lg(obj.__proto__===fn.prototype)//true
+// lg(fn.prototype.constructor=obj)//false   fn
+
+// // lg(fn instanceof obj )//error
 
 
-lg(obj instanceof Function) //false
+// lg(obj instanceof fn)//true
 
-lg( fn instanceof Object) //true
+
+// lg(obj instanceof Function) //false
+
+// lg( fn instanceof Object) //true
+    class Component {
+        constructor(props){
+            this.componentWillRender();
+            this.render();
+            this.componentDidRender();
+        }
+        state=1;
+        componentWillRender=()=>{
+            console.log('componentWillRender')
+        }
+        render=()=>{
+            console.log('render');
+        }
+        componentDidRender=()=>{
+            console.log('componentDidRender')
+        }
+    }
+    new Component();
+    class Child extends Component{
+        constructor(){
+            super();
+            this.num = 10;
+        }
+        state =2;
+        componentDidRender=()=>{
+            console.log('可以改写方法')
+        }
+    }
+    new Child();
