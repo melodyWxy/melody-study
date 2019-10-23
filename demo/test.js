38d2748190f21a9c8741e58f1cf1974ea5917b48
wangxy <wangxy@weipaitang.com>
2019-10-23 16:01:15 +0800

    编辑想

--- demo/test.js
+++ demo/test.js
@@ -1,55 +1,12 @@
-// function lg(...props){
-//     var a = 0;
-//    return  console.log(...props,a++)
-
-
-// }
-
-// function fn(){}
-
-// const obj = new fn(); 
-
-// lg( obj.__proto__ === fn  )  //false
-
-
-// lg(obj.__proto__===fn.prototype)//true
-// lg(fn.prototype.constructor=obj)//false   fn
-
-// // lg(fn instanceof obj )//error
-
-
-// lg(obj instanceof fn)//true
-
-
-// lg(obj instanceof Function) //false
-
-// lg( fn instanceof Object) //true
-    // class Component {
-    //     constructor(props){
-    //         this.componentWillRender();
-    //         this.render();
-    //         this.componentDidRender();
-    //     }
-    //     state=1;
-    //     componentWillRender=()=>{
-    //         console.log('componentWillRender')
-    //     }
-    //     render=()=>{
-    //         console.log('render');
-    //     }
-    //     componentDidRender=()=>{
-    //         console.log('componentDidRender')
-    //     }
-    // }
-    // new Component();
-    // class Child extends Component{
-    //     constructor(props){
-    //         super(props);
-    //         this.num = 10;
-    //     }
-    //     state =2;
-    //     componentDidRender=()=>{
-    //         console.log('可以改写方法')
-    //     }
-    // }
-    // new Child();
+class App extends React.Component{
+    state={
+        a:1
+    }
+    render(){
+        return (
+            <div>
+                {this.state.a}
+            </div>
+        )
+    }
+}
